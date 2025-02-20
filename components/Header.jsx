d5154a5dadf262e54269eb02338ea779fc1370c6
emazack <em.zaccaria@gmail.com>
2025-02-20 11:46:40 +0100

    Add Header component with logo and title; update App and CSS styles

---
+++ components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <img className="logo" src="./images/chef-gpt-icon.png" alt="Icon of the Chef gpt" />
+            <h1 className="title">Chef Gpt</h1>
+        </header>
+    )
+}
